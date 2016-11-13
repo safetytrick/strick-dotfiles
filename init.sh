41f4399d786a9d3b848666398bc94cffd6db92ac
Michael Nielson <michael.nielson@taulia.com>
2016-11-13 13:36:50 -0700

    add global git templates

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ ln -s $@ $DIRECTORY/bashrc $HOME/.bashrc
 ln -s $@ $DIRECTORY/vim $HOME/.vim
 ln -s $@ $DIRECTORY/ctags $HOME/.ctags
 ln -s $@ $DIRECTORY/pythonrc $HOME/.pythonrc
+ln -s $@ $DIRECTORY/git_template $HOME/.git_template
 
 mkdir "$HOME/.vim/swap"
 
@@ -15,9 +16,12 @@ mkdir "$HOME/.vim/swap"
 # no real need to alias this, but standard to always look for ~/.gitignore
 ln -s $@ $DIRECTORY/gitignore_global $HOME/.gitignore
 
+# todo add the rest of my aliases
 git config --global alias.st  status
 git config --global alias.ci  commit
 git config --global alias.co  checkout
 git config --global alias.lg  "log -p"
 git config --global alias.ls  ls-files
+
+git config --global init.templatedir '~/.git_template'
 
